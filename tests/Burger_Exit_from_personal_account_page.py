452dcb1a0b75e9f0af26f6b9870eb8efc05d60db
Inworker <euroman10@mail.ru>
2024-04-16 21:56:34 +0300

    1. Разделил Булки, Соусы, Начинки на 3 теста 2. Перевел класы в CamelCaуs 3. Убрал закомментированый код

--- tests/Burger_Exit_from_personal_account_page.py
+++ tests/Burger_Exit_from_personal_account_page.py
@@ -3,12 +3,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 from locators import BurgerLocator
 
 
-class Test_exit_from_account:
-    def test_exit_acc(self, driver, start_page_login, auth_user_page, start_page_profile):
+class TestExitFromAccount:
+    def test_exit_acc(self, driver, start_page_login, send_data_pole_email,send_data_pole_password, button_enter, start_page_profile):
+
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(BurgerLocator.TITLE_PROFILE))
         driver.find_element(*BurgerLocator.BUTTON_EXIT).click()
 
         WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located(BurgerLocator.TITLE_LOGIN))
         element = driver.find_element(*BurgerLocator.TITLE_LOGIN)
         assert element.text == 'Вход'
-        driver.quit()
+
--- tests/Burger_Register.py
+++ tests/Burger_Register.py
@@ -3,17 +3,16 @@ from selenium.webdriver.support.wait import WebDriverWait
 from locators import BurgerLocator
 
 
-class Test_Burger_Register:
+class TestBurgerRegister:
 
-    def test_successful_registration(self, driver, page_register, input_name, good_password, email_button_register):
+    def test_successful_registration(self, driver, page_register, input_name, good_password, email_input_register,
+                                     button_register):
         WebDriverWait(driver, 5).until(
             expected_conditions.visibility_of_element_located((BurgerLocator.TITLE_LOGIN)))
         Login = driver.find_element(*BurgerLocator.TITLE_LOGIN)
         assert Login.text == "Вход"
-        driver.quit()
 
     def test_unsuccessful_registration_ucorrect_number(self, driver, page_register, input_name, bad_password,
-                                                       email_button_register):
+                                                       email_input_register, button_register):
         title_error_password = driver.find_element(*BurgerLocator.TITLE_BAD_PASSWORD)
         assert title_error_password.is_displayed() and title_error_password.text == "Некорректный пароль"
-        driver.quit()
